ffbde2bd08ba175fd485903df2e73945724c4688
Yifei Shao <shaoyifei96@gmail.com>
2018-04-21 23:12:47 -0400

    finished

---
+++ animate_test.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+
+# fig = plt.figure()
+# ax  = fig.add_subplot(1,1,1)
+# ax.axis([0, 6, 0,6])
+
+
+# x = np.linspace(0, 2 * np.pi, 120)
+# y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
+# # ims is a list of lists, each row is a list of artists to draw in the
+# # current frame; here we are just animating one artist, the image, in
+# # each frame
+# ims = []
+# for i in range(60):
+# 	ax.clear()
+# 	ax.text(i/20., i/10., 'A')
+# 	ax.text(i/10.,i/20.,'B')
+# 	fig.savefig(str(i)+'.png')
+for i in range (128):
+	os.remove('./out/'+str(i)+'.png')
+# ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+#                                 repeat_delay=1000)
+
+# ani.save('dynamic_images.mp4')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
+import matplotlib.patches as patches
 #looks like a dictionary of String: list[string] is easy
 
 def import_grammer_from_file(file_name):
@@ -16,31 +17,8 @@ def import_grammer_from_file(file_name):
 
 	#put into structure 
 	#return grammer for later use
-def CFG_to_CNF(grammer):
-	for key,val in grammer.items():
-		#print(key,val)
-		#1: Check right side S and add S' to production
-		a = ['S' in product.split(' ') for product in val]
-		if any(a):
-			grammer['S^']=['S'] 
-			break
-		#2 Remove NULL production
-		#3 Remove Unit production
-		#4 Remove More than 2 production
-
-		#current example already in CNF form
-	return grammer
-def CKY_parse(sentence,cnf_grammer):
-	x1     =[]
-	y1     =[]
-	color1 =[]
-	text1  =[]#only can have text, the other all have just color
-	x2     =[]
-	y2     =[]
-	color2 =[]
-	x3     =[]
-	y3     =[]
-	color3 =[]
+def CKY_parse(sentence,cnf_grammer,ax):
+	ims=[]
 	split_sentence = sentence.split(' ');
 	length=len(split_sentence)
 # 	#dynamic programming
@@ -60,16 +38,19 @@ def CKY_parse(sentence,cnf_grammer):
 #     set P[1,s,v] = true
 
 # for each l = 2 to n -- Length of span
+	count=0
 	for l in range(2,length+1):
 #   for each s = 1 to n-l+1 -- Start of span
 		for s in range(0,length-l+1):
 #     for each p = 1 to l-1 -- Partition of span
 			for p in range(1,l):
+			
 				for key, value in cnf_grammer.items():
 					#print([length-p,s,length-l+p,s+p]).       #print matrix index
 					if mat[length-p][s] and mat[length-l+p][s+p]:
 						for first_pt in mat[length-p][s]:
 							for second_pt in mat[length-l+p][s+p]:
+								
 								for rule in value:
 									# print('test',first_pt+' '+second_pt)
 									# print('rule',rule)
@@ -77,14 +58,43 @@ def CKY_parse(sentence,cnf_grammer):
 										print(rule)
 										mat[length-l][s].append(key)
 										print_mat(mat)
+
+			#plotting
+				print(length-l,s)
+				count+=1
+				print(count)
+				print(p,s, ' ',l-p,s+p)
+				ax.clear ()
+				#ax.axis('equal')
+				ax.axis([0, length, 0,length])
+				ax.grid(color='black', linestyle='-', linewidth=1)
+				for i in range(length):
+					for j in range(length):
+						if mat[i][j]:
+							ax.text(j+0.4, length-i-0.5, mat[i][j])#print current result
+
+				for i in range(length):
+					ax.text(i+0.5, -0.3, split_sen[i])#print sentence
+				ax.add_patch(patches.Rectangle((s, p-1),1,1,facecolor='#c4dafc'))#first member
+				ax.add_patch(patches.Rectangle((s+p,l-p-1), 1,1,facecolor='#c4dafc'))#second member
+				ax.add_patch(patches.Rectangle((s,length-(length-l)-1), 1,1,facecolor='#619bf9'))#member trying to get
+				x=length
+				y=length
+				for key, value in grammer.items():
+					line = str(key) + str(value)
+					ax.text(x,y, line,fontsize=10,horizontalalignment='right')#print all rules
+
+					y-=.3
+				fig.savefig('./out/'+str(count)+'.png')
+##plotting end	
 #       for each production Ra -> Rb Rc
 				# for key, value in cnf_grammer.items():
 				# 	if(mat[s+1][s])
 	if mat[0][0]:
-		return 1,length, mat
+		return 1,length, mat,ims
 	
 	else:
-		return 0,length, mat
+		return 0,length, mat,ims
 
 #         if P[p,s,b] and P[l-p,s+p,c] then set P[l,s,a] = true
 # if P[n,1,1] is true then
@@ -100,34 +110,42 @@ def print_dict(this_dict):
 		print(key,val)
 
 if __name__ == '__main__':
-	file_name = "short_grammer_1.txt"
-	grammer = import_grammer_from_file(file_name)
-	cnf_grammer = CFG_to_CNF(grammer)
-	print_dict(cnf_grammer)
-	sentence='the duck duck a cat'
-	parse_success, length, mat = CKY_parse(sentence,cnf_grammer)
-	print ('This program is being run by itself')
 
+	sentence='a cat chased the dog in the dog'
+
+	file_name = "short_grammer_1.txt"
+
+	grammer = import_grammer_from_file(file_name)
+#make plotting permanent part
+	split_sen = sentence.split()
+	length=len(split_sen)
+	global fig 
 	fig = plt.figure(0)
 	ax=fig.add_subplot(111)
-	ax.axis([0, length, 0,length])
-	ax.grid(color='r', linestyle='-', linewidth=1)
-	split_sen = sentence.split()
-	for i in range(length):
-		ax.text(i+0.5, -0.3, split_sen[i])
-	for i in range(length):
-		for j in range(length):
-			if mat[i][j]:
-				ax.text(j+0.4, length-i-0.5, mat[i][j])
+	
+	# for i in range(length):
+	# 	ax.text(i+0.5, -0.3, split_sen[i])
+	# x=length-0.05
+	# y=length-0.15
+	# for key, value in grammer.items():
+	# 	line = str(key) + str(value)
+	# 	ax.text(x,y, line,fontsize=10,horizontalalignment='right')
+	# 	y-=.2
+	
 
-	x=length-0.05
-	y=length-0.15
-	for key, value in grammer.items():
-		line = str(key) + str(value)
-		ax.text(x,y, line,fontsize=10,horizontalalignment='right')
-		y-=.2
+	parse_success, length, mat,ims = CKY_parse(sentence,grammer,ax)
+	# for i in range(length):
+	# 	for j in range(length):
+	# 		if mat[i][j]:
+	# 			ax.text(j+0.4, length-i-0.5, mat[i][j])
+
 		
-	plt.show()
+	# ani = animation.ArtistAnimation(fig, ims, interval=100, blit=False,
+ #                                repeat_delay=1000)
+
+# ani.save('dynamic_images.mp4')
+
+	# plt.show()
 
 
 else:
